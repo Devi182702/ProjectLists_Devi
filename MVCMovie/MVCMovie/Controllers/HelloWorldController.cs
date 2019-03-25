7e808c5ce73ac9477e1ca1b21e84b1e8792bd5c8
Devi182702 <48868286+Devi182702@users.noreply.github.com>
2019-03-25 00:08:58 -0400

    Initial Commit

---
+++ MVCMovie/MVCMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,18 @@
+﻿using System.Web.Mvc;
+
+namespace MVCMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // GET: HelloWorld
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public string Welcome()
+        {
+            return "this is from Welcome Hellowrold";
+        }
+    }
+}
